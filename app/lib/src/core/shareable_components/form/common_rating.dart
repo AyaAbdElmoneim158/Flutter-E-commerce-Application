e12c7acfe827a0550bca8cbd690f25c0096be79a
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2023-12-25 03:42:07 +0200

    🔸 Add shareable_components `Forms_components`

---
+++ app/lib/src/core/shareable_components/form/common_rating.dart
@@ -0,0 +1,43 @@
+import 'package:app/src/core/theme/app_theme.dart';
+import 'package:app/src/core/utils/app_colors.dart';
+import 'package:app/src/core/constance.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class CommonRating extends StatefulWidget {
+  const CommonRating({super.key});
+
+  @override
+  State<CommonRating> createState() => _CommonRatingState();
+}
+
+class _CommonRatingState extends State<CommonRating> {
+  double rating = 0;
+  @override
+  Widget build(BuildContext context) {
+    return RatingBar.builder(
+      initialRating: 3,
+      minRating: 1,
+      direction: Axis.horizontal,
+      allowHalfRating: true,
+      itemCount: 5,
+      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+      itemBuilder: (context, index) => (index < rating)
+          ? const Icon(
+              Icons.star_rounded,
+              color: AppColors.ratingStarColor,
+            )
+          : Icon(
+              Icons.star_border_rounded,
+              color: AppTheme.getColor(ColorType.gray, Constance.isLight),
+            ),
+      glow: false,
+      onRatingUpdate: (val) {
+        debugPrint(val.toString());
+        setState(() {
+          rating = val;
+        });
+      },
+    );
+  }
+}
---
+++ app/lib/src/core/shareable_components/form/common_tag.dart
@@ -0,0 +1,80 @@
+import 'package:app/src/core/theme/app_theme.dart';
+import 'package:app/src/core/constance.dart';
+import 'package:app/src/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CommonTag extends StatelessWidget {
+  const CommonTag({
+    super.key,
+    required this.tagText,
+    this.styleNumber = 1,
+  });
+  final String? tagText;
+  final int styleNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: (styleNumber == 4) ? 35 : 40,
+      width: 100,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      decoration: BoxDecoration(
+        color: getBgColor(styleNumber),
+        borderRadius: (styleNumber == 4)
+            ? BorderRadius.circular(29)
+            : BorderRadius.circular(8),
+        border: (styleNumber != 1)
+            ? null
+            : Border.all(
+                width: 1.3,
+                color: AppTheme.getColor(
+                  ColorType.gray,
+                  Constance.isLight,
+                ),
+              ),
+      ),
+      child: Text(
+        tagText!,
+        style: Styles()
+            .getText14pxTextStyle(
+              color: getColor(styleNumber),
+            )
+            .copyWith(
+              fontWeight: FontWeight.w600,
+            ),
+      ),
+    );
+  }
+
+  Color getColor(styleNumber) {
+    if ((styleNumber == 1) || (styleNumber == 3)) {
+      return AppTheme.getColor(
+        ColorType.text,
+        Constance.isLight,
+      );
+    } else if ((styleNumber == 2) || (styleNumber == 4)) {
+      return AppTheme.getColor(
+        ColorType.text,
+        !Constance.isLight,
+      );
+    } else {
+      return AppTheme.getColor(
+        ColorType.success,
+        Constance.isLight,
+      );
+    }
+  }
+
+  Color getBgColor(styleNumber) {
+    if (styleNumber == 1) {
+      return AppTheme.getColor(ColorType.background, Constance.isLight);
+    } else if (styleNumber == 2) {
+      return AppTheme.getColor(ColorType.primary, Constance.isLight);
+    } else if (styleNumber == 3) {
+      return AppTheme.getColor(ColorType.background, Constance.isLight);
+    } else {
+      return AppTheme.getColor(ColorType.text, Constance.isLight);
+    }
+  }
+}
