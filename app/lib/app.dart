19338fd784e6c211f08872c5f8094c11a3712eb3
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2023-12-30 10:39:44 +0200

    🔸 Edit General

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:app/src/core/constance.dart';
 import 'package:app/src/core/theme/app_theme.dart';
 import 'package:app/src/core/utils/size_config.dart';
-import 'package:app/src/screens/main_screen/category/category_screen.dart';
+import 'package:app/src/screens/main_screen/category/category_details_screen.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -19,7 +19,21 @@ class App extends StatelessWidget {
       darkTheme: AppTheme.getThemeData(false),
       // onGenerateRoute: AppRouter.onGenerate,
       // initialRoute: Routes.initRoute,
-      home: const CategoryScreen(),
+      home:
+          /* Scaffold(
+        body: Center(
+          child: Text(
+            "Hi, there",
+            style: Styles().getHeadline2TextStyle(
+              color: AppTheme.getColor(
+                ColorType.text,
+                Constance.isLight,
+              ),
+            ),
+          ),
+        ),
+      ),*/
+          const CategoryDetailsScreen(),
     );
   }
 }
